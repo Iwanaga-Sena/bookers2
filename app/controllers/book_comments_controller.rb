0a3d810f86496c561bd9febf37b55f8920e88904
Iwanaga-Sena <171421410+Iwanaga-Sena@users.noreply.github.com>
2024-07-10 21:07:59 +0900

    [fix] path

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,14 +4,14 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book.id)
+    redirect_to book_path(book)
   end
 
   def destroy
     book = Book.find(params[:book_id])
     comment = BookComment.find(params[:id])
     comment.destroy
-    redirect_to books_path(book.id)
+    redirect_to books_path(book)
   end
 
   private
